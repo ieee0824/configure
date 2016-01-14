1a2aafd7698e09a7ed136abfabaf9e493cc974ec
shirase_aoi <ieee0824@ezweb.ne.jp>
2016-01-15 00:07:04 +0900

    conf test

--- conf.go
+++ conf.go
@@ -1,5 +1,10 @@
 package configure
 
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
 type Conf struct {
 	c map[string]interface{}
 }
@@ -12,6 +17,28 @@ func (c *Conf) Get(k string) interface{} {
 	return c.c[k]
 }
 
-func NewConf(path string) *Conf {
+func (c *Conf) Write(path string) error {
+	j, err := json.Marshal(c.c)
+
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(path, j, 0644)
+}
+
+func (c *Conf) Read(path string) {
+	bin, err := ioutil.ReadFile(path)
+	if err != nil {
+		c.c = nil
+		return
+	}
+	err = json.Unmarshal(bin, &c.c)
+	if err != nil {
+		c.c = nil
+		return
+	}
+}
+
+func NewConf() *Conf {
 	return &Conf{map[string]interface{}{}}
 }
--- conf_test.go
+++ conf_test.go
@@ -2,11 +2,13 @@ package configure
 
 import (
 	"io/ioutil"
+	"os"
+	"reflect"
 	"testing"
 )
 
 func TestNewConf(t *testing.T) {
-	c := NewConf("./test/conf.json")
+	c := NewConf()
 
 	if c == nil {
 		fileNames := []string{}
@@ -15,5 +17,51 @@ func TestNewConf(t *testing.T) {
 			fileNames = append(fileNames, f.Name())
 		}
 		t.Fatal(fileNames)
+	}
+}
+
+func TestSet(t *testing.T) {
+	c := NewConf()
+	c.Set("loc", "tokyo")
+	if c.c["loc"] != "tokyo" {
+		t.Fatal(c.c)
+	}
+}
+
+func TestGet(t *testing.T) {
+	c := NewConf()
+	c.c["loc"] = "akiba"
+	loc := c.Get("loc")
+
+	s, ok := loc.(string)
+	if !ok || s != "akiba" {
+		t.Fatal(ok, "'"+s+"'")
+	}
+}
+
+func TestWrite(t *testing.T) {
+	JSON := "{\"loc\":\"kanda\"}"
+	c := NewConf()
+	c.c["loc"] = "kanda"
+	err := c.Write("./test/tmp.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	bin, err := ioutil.ReadFile("./test/tmp.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !reflect.DeepEqual(bin, []byte(JSON)) {
+		t.Fatal(string(bin), JSON)
+	}
+	os.Remove("./test/tmp.json")
+}
+
+func TestRead(t *testing.T) {
+	c := NewConf()
+	c.Read("./test/conf.json")
+
+	if c.c == nil {
+		t.Fatal(c.c)
 	}
 }
