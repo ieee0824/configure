34dcbb1e7ae0017e0cc18928c88ec9e9d6573d0a
shirase_aoi <ieee0824@ezweb.ne.jp>
2016-01-17 03:39:46 +0900

    include test

--- conf_test.go
+++ conf_test.go
@@ -5,8 +5,22 @@ import (
 	"testing"
 )
 
-func TestNewConf(t *testing.T) {
+var includeList = []string{
+	"conf3.json",
+	"./conf3.json",
+	"../test/conf3.json",
+}
 
+func isExist(list []string, s string) bool {
+	for _, v := range list {
+		if v == s {
+			return true
+		}
+	}
+	return false
+}
+
+func TestNewConf(t *testing.T) {
 	if NewConf("./test/conf.json") == nil {
 		fileNames := []string{}
 		files, _ := ioutil.ReadDir("./test")
@@ -15,6 +29,24 @@ func TestNewConf(t *testing.T) {
 		}
 		t.Log("can not allocated.")
 		t.Fatal(fileNames)
+	}
+	if s, ok := NewConf("./test/conf.json").c["hoge"].(string); ok {
+		if s != "hage" {
+			t.Log(ok, "\n")
+			t.Log(NewConf("./test/conf.json"), "\n")
+			t.Log(NewConf("./test/conf.json").c["hoge"], "\n")
+			t.Log(s, "\n")
+			t.Fail()
+		}
+	}
+	if s, ok := NewConf("./test/conf.json").c["hoge"].(string); !ok {
+		if s == "hage" {
+			t.Log(ok, "\n")
+			t.Log(NewConf("./test/conf.json"), "\n")
+			t.Log(NewConf("./test/conf.json").c["hoge"], "\n")
+			t.Log(s, "\n")
+			t.Fail()
+		}
 	}
 	if NewConf("") != nil {
 		t.Log("file name validate test.")
@@ -46,5 +78,68 @@ func TestGet(t *testing.T) {
 	s, ok := loc.(string)
 	if !ok || s != "akiba" {
 		t.Fatal(ok, "'"+s+"'")
+	}
+}
+
+func TestGetKeys(t *testing.T) {
+	t.Log("TestGetKeys\n")
+	normal := NewConf("./test/conf.json")
+	if len(normal.GetKeys()) == 0 {
+		t.Log("can not get keys.\n")
+		t.Log("length is zero.")
+		t.Fail()
+	}
+	if func() bool {
+		for _, key := range normal.GetKeys() {
+			if key == "hoge" {
+				return false
+			}
+		}
+		return true
+	}() {
+		t.Log(normal.GetKeys(), "\n")
+		t.Fail()
+	}
+	abnormal := NewConf("./test/conf2.json")
+	if len(abnormal.GetKeys()) != 0 {
+		t.Log("can not get keys.\n")
+		t.Log("length is not zero.")
+		t.Fail()
+	}
+}
+
+func TestGetIncludeFileNames(t *testing.T) {
+	t.Log("TestGetIncludeFileNames\n")
+	normal := NewConf("./test/conf.json")
+	if len(normal.getIncludeFileNames()) == 0 || len(normal.getIncludeFileNames()) != 3 {
+		t.Log("can not get include path.")
+		t.Log("len", len(normal.getIncludeFileNames()))
+		t.Fail()
+	}
+	for _, l := range includeList {
+		if !isExist(normal.getIncludeFileNames(), l) {
+			t.Log("is not existed.")
+			t.Fail()
+		}
+	}
+	abnormal := NewConf("./test/conf2.json")
+	if len(abnormal.getIncludeFileNames()) != 0 || abnormal.getIncludeFileNames() != nil {
+		t.Log(abnormal)
+		t.Fail()
+	}
+}
+
+func TestInclude(t *testing.T) {
+	normal := NewConf("./test/conf.json")
+	normal.include("./test/conf3.json")
+	if normal.Get("loc").(string) != "tokyo" {
+		t.Log(normal.Get("loc"))
+		t.Fail()
+	}
+	abnormal := NewConf("./test/conf2.json")
+	abnormal.include("./test/conf3.json")
+	if _, ok := abnormal.Get("loc").(string); ok {
+		t.Log(abnormal.Get("loc"))
+		t.Fail()
 	}
 }
