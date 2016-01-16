403675613989c243ce8d643d61115735db6d0925
shirase_aoi <ieee0824@ezweb.ne.jp>
2016-01-17 00:29:28 +0900

    コメント追加

--- conf.go
+++ conf.go
@@ -5,18 +5,22 @@ import (
 	"io/ioutil"
 )
 
+// Conf to hold the configure that you read.
 type Conf struct {
 	c map[string]interface{}
 }
 
+// Set additional configuration items.
 func (c *Conf) Set(k string, v interface{}) {
 	c.c[k] = v
 }
 
+// Get is to get the configuration item.
 func (c *Conf) Get(k string) interface{} {
 	return c.c[k]
 }
 
+// NewConf to get the configuration item from json.
 func NewConf(filePath string) *Conf {
 	if filePath == "" {
 		return nil
